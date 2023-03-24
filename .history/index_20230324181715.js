3416bfb68cd019df612c471e45c045c1117a66b5
Siva20021 <88962500+Siva20021@users.noreply.github.com>
2023-03-24 18:25:17 +0530

    Notifications is enabled

---
+++ .history/index_20230324181715.js
@@ -0,0 +1,278 @@
+const apiKey = "9069ea7df5255bddc76e81b66070ca9a";
+history.pushState(null, null, location.href);
+window.onpopstate = function () {
+  history.go(1);
+};
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const searchBtn = document.getElementById("searchBtn");
+const currentWeather = document.getElementById("myLocationBtn");
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+async function getWeatherByLocation(city) {
+  const resp = await fetch(url(city), {
+    origin: "cros",
+  });
+  const respData = await resp.json();
+
+  addWeatherToPage(respData);
+  console.log(respData);
+}
+const myLocationBtn = document.getElementById("myLocationBtn");
+// function handleOpen() {
+//   <Modal>
+//     <h1>Hello</h1>
+//   </Modal>;
+// }
+myLocationBtn.addEventListener("click", () => {
+  getWeatherByMyLocation();
+});
+function getWeatherByMyLocation() {
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    const { latitude, longitude } = position.coords;
+    const resp = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    );
+    const respData = await resp.json();
+    // addWeatherToPage(respData);
+    const temp = Ktoc(respData.main.temp);
+
+    // Update the currentWeather element's text content with the temperature in Celsius
+    currentWeather.textContent = temp + "°C";
+  });
+}
+
+window.addEventListener("load", function () {
+  // Select a random number between 1 and 12
+  var randomBg = Math.floor(Math.random() * 12) + 1;
+
+  // Set the body background to the random image
+  document.body.style.backgroundImage =
+    "url('backgrounds/bg-" + randomBg + ".jpg')";
+});
+
+function addWeatherToPage(data) {
+  // handle error case where main property is not defined
+  if (!data.main) {
+    const weatherContainer = document.getElementById("weather-container");
+    weatherContainer.innerHTML = "<div class='weather'>City not found</div>";
+    return;
+  }
+
+  const temp = Ktoc(data.main.temp);
+
+  const weatherContainer = document.getElementById("weather-container");
+  weatherContainer.innerHTML = `
+    <div class="weather">
+      <h2>
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" />
+      </h2>
+      <small>${data.weather[0].main}</small>
+      <br/>
+      <span>humidity:${data.main.humidity}</span>
+      <span>wind:${data.wind.speed}</span>
+
+
+
+      
+    
+    
+    </div>
+  `;
+}
+
+function Ktoc(K) {
+  return Math.floor(K - 273.15);
+}
+const weatherForecast = document.querySelector(".weather-forecast");
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = search.value;
+  weatherForecast.style.display = "block";
+  if (city) {
+    getWeatherByLocation(city);
+    getWeatherForNext7days(city);
+  }
+});
+
+function getWeatherForNext7days(city) {
+  const ENDPOINT = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&cnt=7`;
+
+  console.log(ENDPOINT);
+
+  const weatherCards = document.querySelectorAll(".weather-card");
+
+  fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      const forecasts = data.list;
+      // Use the entire forecast data for all 7 days
+
+      forecasts.forEach((forecast, index) => {
+        const weatherCard = weatherCards[index];
+        const date = new Date(forecast.dt * 1000);
+        const options = { weekday: "long", day: "numeric" };
+        const dayOfWeek = date.toLocaleDateString("en-US", options);
+        const iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+
+        weatherCard.querySelector(".icon img").src = iconUrl;
+        weatherCard.querySelector(".description").textContent =
+          forecast.weather[0].description;
+        weatherCard.querySelector(
+          ".temp"
+        ).textContent = `${forecast.main.temp.toFixed(0)}°C`;
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
+function requestNotificationPermission() {
+  Swal.fire({
+    title: "Do you want to enable notifications?",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: `Allow`,
+    denyButtonText: `Don't allow`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          Swal.fire("Notifications enabled!", "", "success");
+          showNotification();
+        } else {
+          Swal.fire("Notifications denied", "", "error");
+        }
+      });
+    } else if (result.isDenied) {
+      Swal.fire("Notifications denied", "", "error");
+    }
+  });
+}
+console.log(getWeatherByMyLocation);
+
+async function showNotification() {
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    const { latitude, longitude } = position.coords;
+    const resp = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+    );
+    const respData = await resp.json();
+    const temp = Ktoc(respData.main.temp);
+
+    let weatherCondition = "";
+    respData.weather.forEach((condition) => {
+      if (condition.main === "Rain") {
+        weatherCondition = "rainy";
+        Swal.fire({
+          title: "Notification title",
+          text: `The current temperature is ${temp}°C and it is ${weatherCondition}`,
+          icon: "success",
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timerProgressBar: true,
+        });
+      } else if (condition.main === "Drizzle") {
+        weatherCondition = "drizzling";
+        Swal.fire({
+          title: "Notification title",
+          text: `The current temperature is ${temp}°C and it is ${weatherCondition}`,
+          icon: "success",
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timerProgressBar: true,
+        });
+      } else {
+        weatherCondition = "Fine";
+      }
+    });
+
+    Swal.fire({
+      title: "Notification title",
+      text: `The current temperature is ${temp}°C and it is ${weatherCondition}`,
+      icon: "success",
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timerProgressBar: true,
+    });
+  });
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByTagName("button")[0];
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const cookieValue = parts.pop().split(";").shift();
+    return cookieValue.split(",");
+  }
+}
+
+const cookieValue = getCookie("access_token");
+document.getElementById("UserName").textContent = cookieValue[0];
+document.getElementById("UserEmail").textContent = cookieValue[1];
+document.getElementById("Usercity1").textContent = cookieValue[2];
+document.getElementById("Usercity2").textContent = cookieValue[3];
+document.getElementById("Usercity3").textContent = cookieValue[4];
+
+const preferredCities = [cookieValue[2], cookieValue[3], cookieValue[4]];
+console.log(preferredCities);
+function checkWeatherAndAlert() {
+  preferredCities.forEach((city) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    )
+      .then((response) => console.log())
+      .then((response) => response.json())
+      .then((data) => {
+        const isRaining = data.weather[0].main.toLowerCase().includes("rain");
+        const w = data.weather[0].main.toLowerCase();
+        console.log(isRaining);
+        if (isRaining) {
+          Swal.fire({
+            title: "Notification title",
+            text: `Its raining in ${city}°C and it is ${weatherCondition}`,
+            icon: "warning",
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timerProgressBar: true,
+          });
+        }
+      })
+      .catch((error) => console.error(error));
+  });
+}
+
+setInterval(checkWeatherAndAlert, 24 * 60 * 60 * 1000);
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException, Depends, Header
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import psycopg2
 import bcrypt
+import jwt
+from datetime import datetime, timedelta
+import secrets
 app = FastAPI()
+
+
+# Allow CORS for development purposes
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
 ]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Replace * with the domains you want to allow
@@ -17,6 +26,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 # Database connection
 conn = psycopg2.connect(
     host="localhost",
@@ -32,13 +42,46 @@ class User(BaseModel):
     email: str
     password: str
     name: str
-
+    city: str
+    city2: str
+    city3: str
 # LoginUser model
 
 
 class LoginUser(BaseModel):
     email: str
     password: str
+
+
+# Get JWT token from header
+JWT_SECRET = secrets.token_hex(32)
+
+
+async def get_token(authorization: str = Header(...)):
+    if not authorization:
+        raise HTTPException(
+            status_code=401, detail="Invalid authentication credentials")
+    scheme, token = authorization.split()
+    if scheme.lower() != "bearer":
+        raise HTTPException(
+            status_code=401, detail="Invalid authentication credentials")
+    return token
+
+# Verify JWT token and get current user
+
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(
+                status_code=401, detail="Invalid authentication credentials")
+        return email
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+# Sign up new user
 
 
 @app.post("/signup")
@@ -56,12 +99,15 @@ def signup(user: User):
 
     # Insert user into database
     cursor.execute(
-        "INSERT INTO users (email, password,name) VALUES (%s, %s, %s)",
-        (user.email, hashed_password.decode(), user.name)
+        "INSERT INTO users (email, password, name,city,city2,city3) VALUES (%s, %s, %s,%s, %s, %s)",
+        (user.email, hashed_password.decode(),
+         user.name, user.city, user.city2, user.city3)
     )
     conn.commit()
 
     return {"message": "User created successfully"}
+
+# Log in existing user and generate JWT token
 
 
 @app.post("/login")
@@ -70,7 +116,7 @@ def login(user: LoginUser):
 
     # Retrieve user from database
     cursor.execute(
-        "SELECT email, password FROM users WHERE email=%s", (user.email,))
+        "SELECT email, password, name FROM users WHERE email=%s", (user.email,))
     db_user = cursor.fetchone()
 
     if db_user is None:
@@ -79,7 +125,31 @@ def login(user: LoginUser):
 
     # Verify password
     if bcrypt.checkpw(user.password.encode(), db_user[1].encode()):
-        return {"message": "Login successful"}
+        # Generate JWT token
+        token = jwt.encode({
+            "sub": db_user[0],
+            "iat": datetime.utcnow(),
+            "exp": datetime.utcnow() + timedelta(hours=1),
+        }, JWT_SECRET, algorithm="HS256")
+
+        # Decode JWT token to get user email
+        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(
+                status_code=401, detail="Invalid authentication credentials")
+
+        # Retrieve user details from database
+        cursor.execute(
+            "SELECT email, name,city,city2,city3 FROM users WHERE email=%s", (email,))
+        current_user = cursor.fetchone()
+
+        return {"access_token": current_user}
     else:
         raise HTTPException(
             status_code=400, detail="Incorrect email or password")
+
+
+@app.post("/protected")
+def protected(current_user: str = Depends(get_current_user)):
+    return {"message": f"Hello {current_user}"}
